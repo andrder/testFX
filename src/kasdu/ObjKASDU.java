58a12522d2275fbec9cbcdcf245abbe0b9276a6c
Андрей <andrd08@yandex.ru>
2016-12-29 00:40:50 +0300

    дописан код

--- src/kasdu/ObjKASDU.java
+++ src/kasdu/ObjKASDU.java
@@ -1,14 +1,30 @@
 package kasdu;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
- * Created by Андрей on 26.12.2016.
+ * Устройство КАСДУ с набором ТС,ТУ, и, возможно вложенными такими же объектами (т.е. группа)
  */
 public class ObjKASDU {
-    ArrayList<SignalTS> ListTS;
-    ArrayList<CommandTU> ListTU;
-    String qwe;
+    private final String name;
+    private List<ObjKASDU> listObj = new LinkedList<>(); //Список вложенных объектов экземпляра
+    private List<SignalTS> listTS = new LinkedList<>();//сигналы ТС экземпляра
+    private List<CommandTU> listTU = new LinkedList<>(); //команды ТУ экземпляра
 
+    public ObjKASDU(String name) {
+        this.name = name;
+    }
 
+    public void addObj (ObjKASDU obj) {
+        listObj.add(obj);
+    }
+
+    public void addTS (SignalTS signalTS){
+        listTS.add(signalTS);
+    }
+
+    public void addTU (CommandTU commandTU){
+        listTU.add(commandTU);
+    }
 }
